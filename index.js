e9b08064a8c75a253f49447bf04ac94b07d1924d
Zambom <erikzambom@hotmail.com>
2024-08-29 18:27:33 -0300

    feat: adding package informations

---
+++ index.js
@@ -0,0 +1,10 @@
+const asyncFilter = require("./resources/filter")
+const asyncMap = require("./resources/map")
+const { delay } = require("./resources/utils")
+
+
+module.exports = {
+  asyncFilter,
+  asyncMap,
+  delay
+}
